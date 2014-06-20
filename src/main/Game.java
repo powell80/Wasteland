65182f1b15516ea991bef73fc5d77f113a981353
Tyler Powell <tylerpowell89@gmail.com>
2014-06-20 11:52:06 -0700

    second commit

---
+++ src/main/Game.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable
+{
+	private static final long serialVersionUID = 1L;
+
+	public void init()
+	{
+			
+	}
+		
+	public void run()
+	{
+			
+	}
+	
+
+	public static void main(String[] args) 
+	{
+		Game game = new Game();
+
+	}
+}
+
