33bec555c51d6a9dbf9258985f0d63612d278a06
Tyler Powell <tylerpowell89@gmail.com>
2014-06-22 22:53:31 -0700

    added methods to Game class

--- src/main/Game.java
+++ src/main/Game.java
@@ -2,19 +2,75 @@ package main;
 
 import java.awt.Canvas;
 
+import javax.swing.JFrame;
+
 public class Game extends Canvas implements Runnable
 {
 	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 512, HEIGHT = 384, SCALE = 2;
+	public static boolean running = false;
+	public static boolean ladyDialog = false;
+	public static boolean ladyDialog2 = false;
+	public static int dialogBool=0;
+	public Thread gameThread;
+	public static  JFrame frame;
 
 	public void init()
 	{
 			
 	}
+	
+	public synchronized void start()
+	{
+		if(running)return;
+		running = true;
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	public synchronized void stop()
+	{
+		if(!running)return;
+		running = false;
+		try {
+			gameThread.join();
+		} catch (InterruptedException e) {e.printStackTrace();}
+	}
+	
 		
 	public void run()
 	{
-			
+		init();
+		long lastTime = System.nanoTime();
+		final double amountOfTicks = 60D;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		
+		while(running)
+		{
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if(delta >= 1)
+			{
+				tick();
+				delta--;
+			}
+			render();
+		}
+		stop();
 	}
+	
+	public void tick()
+	{
+		
+	}
+	
+	public void render()
+	{
+		
+	}
+
 	
 
 	public static void main(String[] args) 
