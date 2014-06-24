3252c694b808f20aef77526c1f250bf3f38ee41f
Tyler Powell <tylerpowell89@gmail.com>
2014-06-23 19:07:00 -0700

    commiting from desktop

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -1,5 +1,15 @@
 package main;
 
-public class Menu {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 
+public class Menu 
+{
+	public void render(Graphics g)
+	{
+		Font fnt0 = new Font("", Font.BOLD, 50);
+		g.setFont(fnt0);
+		g.setColor(Color.white);
+	}
 }
