95e26373f4869dda8206b4c347ab3bd03f3a3ecd
Tyler Powell <tylerpowell89@gmail.com>
2014-06-28 22:13:02 -0700

    committing changes to menuButton class

--- src/menuPack/MenuButton.java
+++ src/menuPack/MenuButton.java
@@ -1,20 +1,28 @@
 package menuPack;
 
+import java.awt.geom.Rectangle2D;
+
 public class MenuButton 
 {
 	private int width;
 	private int height;
 	private int x;
 	private int y;
+	Rectangle2D rect;
 	
 	public MenuButton(int x, int y, int height, int width)
 	{
 		this.x = x;
 		this.y = y;
-		this.width = width;
 		this.height = height;
+		this.width = width;
 	}
 	
+	public Rectangle2D rectBounds()
+	{
+		rect.setRect(x, y, width, height);
+		return rect;
+	}
 	
 	
 }
