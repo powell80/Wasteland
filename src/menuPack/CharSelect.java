9e0261ea56064258011cc1ec7049a5af5164194c
Tyler Powell <tylerpowell89@gmail.com>
2014-07-02 21:37:44 -0700

    added charSelect class for character selection screen

---
+++ src/menuPack/CharSelect.java
@@ -0,0 +1,21 @@
+package menuPack;
+
+import java.awt.Graphics;
+
+public class CharSelect 
+{
+	public CharSelect()
+	{
+		
+	}
+	
+	public void tick()
+	{
+		
+	}
+	
+	public void Render(Graphics g)
+	{
+		
+	}
+}
